253190635e5b5e47625e3f68c1498f566e9c219e
John Francis <jfcortez1997Agmail.com>
2021-06-03 12:04:06 +0800

    add src folder

--- continuous integration/action_thru_push/test/test.js
+++ continuous integration/action_thru_push/test/test.js
@@ -1,5 +1,5 @@
-const WorkingDirectory = require("../models/working-directory");
-const GitCommand = require("../models/git-command");
+const WorkingDirectory = require("../src/models/working-directory");
+const GitCommand = require("../src/models/git-command");
 
 const chai = require('chai');
 const expect = chai.expect;
