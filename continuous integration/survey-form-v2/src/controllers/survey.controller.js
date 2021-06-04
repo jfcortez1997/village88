a000df4cc4a6204249a184f4df70ffc473eed818
John Francis <jfcortez1997Agmail.com>
2021-06-04 18:12:15 +0800

    double check jenkins, must only execute on PR

--- continuous integration/survey-form-v2/src/controllers/survey.controller.js
+++ continuous integration/survey-form-v2/src/controllers/survey.controller.js
@@ -31,6 +31,6 @@ class SurveyController extends Controller {
     async process(req, res) {
         await SurveyModel.createSurvey(req.body.name, req.body.dojo_location, req.body.fave_lang, req.body.comment);
         res.redirect("/success");
-    } 
+    }
 }
 module.exports = SurveyController;
