187171debc6274efc6a86e6beb380d20905f167d
John Francis <jfcortez1997Agmail.com>
2021-06-04 21:32:19 +0800

    replace

---
+++ advance git/redo_pull_request/pull_request_3/survey-form-v1/models/survey.model.js
@@ -0,0 +1,68 @@
+const Mysql                 = require('mysql');
+const Model 				= require('./model');
+const Constants 			= require('../config/constants');
+
+class SurveyModel extends Model {
+	constructor() {
+		super();
+		this.captcha = null;
+	}
+
+	async getSurvey() {
+		let response_data 	    = {status: false, result: [], err: null};
+		
+		try{
+			let get_survey_query 		= Mysql.format(`
+				SELECT surveys.id, surveys.name, surveys.location, surveys.favorite_language, surveys.comment
+				FROM surveys
+				ORDER BY surveys.id DESC
+				LIMIT 1;`
+			);
+
+			let [get_survey_result] = await this.executeQuery(get_survey_query);
+	
+			if(get_survey_result){
+				response_data.status 	= true;
+				response_data.result 	= get_survey_result;
+			}else{
+				response_data.message 	= "No survey found";
+			}
+		}catch(err){
+			response_data.err 			= err;
+			response_data.message 		= "Error in getting a survey.";
+		};
+	
+		return response_data;		
+	}
+
+	async createSurvey(name, dojo_location, fave_lang, comment){
+		let insert_survey_query = Mysql.format(`
+				INSERT INTO surveys(surveys.name, surveys.location, surveys.favorite_language, surveys.comment)
+				VALUES ('${name}', '${dojo_location}',
+				'${fave_lang}', '${comment}');`
+			);
+		await this.executeQuery(insert_survey_query);
+	}
+
+	// supply the logic for each function:
+	generateCaptcha(){
+		var character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890" 
+		var captcha = '';
+		for(var i = 0; i < 6; i++){
+			var index = Math.floor(Math.random()*character.length);
+			captcha += character[index];
+		}
+		this.captcha = captcha;
+		return captcha; 
+	}
+
+	verifyCaptchaInput(input){
+		if(input  == this.captcha){
+			return "Success! Captcha input matched.";
+		}else{
+			return "Error! Captcha input doesn't matched.";
+		}
+	}
+}
+
+module.exports = SurveyModel;
